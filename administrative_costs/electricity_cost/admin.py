0dd583f2980f2752f3504378a64bcd61fae56f73
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-03-09 21:41:38 +0100

    dodanie tabeli z odczytami licznikow

--- administrative_costs/electricity_cost/admin.py
+++ administrative_costs/electricity_cost/admin.py
@@ -1,23 +1,46 @@
 from django.contrib import admin
-from .models import EnergyMeters, EnergySuppliers, Invoices, MeterLocations
+from .models import EnergyMeters, EnergySuppliers, Invoices, MeterLocations, FinancialEntity, MeterReading, Month, Year
 
 
 class InvoicesAdmin(admin.ModelAdmin):
-    list_display = ['invoices_number', 'cost', 'cost_per_1_mwh', 'numbers_mwh', 'energysuppliers', 'month', 'year']
-    list_filter = ['invoices_number', 'month', 'year']
+    list_display = ['invoices_number', 'cost', 'cost_per_1_mwh', 'numbers_mwh', 'energysuppliers', 'biling_month',
+                    'biling_year']
+    list_filter = ['invoices_number', 'biling_month', 'biling_year']
     search_fields = ['invoices_number', 'cost']
+
 
 class EnergyMetersAdmin(admin.ModelAdmin):
     list_display = ['name', 'meter_location', 'museum_share', 'cob_share', 'parish_share', 'institute_share']
+    list_filter = ['name', 'meter_location']
+    search_fields = ['name']
+    search_help_text = 'Szukaj po nazwie licznika'
+
 
 class MeterLocationsAdmin(admin.ModelAdmin):
     list_display = ['name']
 
-class Energy_SuppliersAdmin(admin.ModelAdmin):
+
+class EnergySuppliersAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class MeterReadingAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in MeterReading._meta.fields]
+    list_filter = ['biling_month', 'biling_year', 'energy_meter']
+
+class MonthAdmin(admin.ModelAdmin):
+    list_display = ['name', 'number_of_month']
+
+class YearAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
 admin.site.register(Invoices, InvoicesAdmin)
 admin.site.register(MeterLocations, MeterLocationsAdmin)
-admin.site.register(EnergySuppliers, Energy_SuppliersAdmin)
+admin.site.register(EnergySuppliers, EnergySuppliersAdmin)
 admin.site.register(EnergyMeters, EnergyMetersAdmin)
+admin.site.register(FinancialEntity)
+admin.site.register(MeterReading, MeterReadingAdmin)
+admin.site.register(Month, MonthAdmin)
+admin.site.register(Year, YearAdmin)
+
