37b9d4a9b4c559c7f2aa412d38716a0157c79fea
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-03-11 22:54:39 +0100

    pierwsze podejscie do listy faktur i pliku main.html

--- administrative_costs/electricity_cost/models.py
+++ administrative_costs/electricity_cost/models.py
@@ -91,7 +91,7 @@ class Year(models.Model):
 
 
 class MeterReading(models.Model):
-    energy_meter = models.ForeignKey('EnergyMeters', on_delete=models.CASCADE, verbose_name='Licznik')
+    energy_meter = models.ForeignKey('EnergyMeters', on_delete=models.PROTECT, verbose_name='Licznik')
     date_of_reading = models.DateField(verbose_name='Data odczytu')
     meter_reading = models.FloatField(verbose_name='Odczyt licznika')
     biling_month = models.ForeignKey('Month', verbose_name='Miesiąc rozliczeniowy', on_delete=models.PROTECT)
@@ -99,3 +99,15 @@ class MeterReading(models.Model):
 
     def __str__(self):
         return f'''Odczyt licznika {self.energy_meter} z dnia {self.date_of_reading}'''
+
+class CounterUsage(models.Model):
+    energy_meter = models.ForeignKey('EnergyMeters', on_delete=models.PROTECT, verbose_name='Licznik')
+    biling_month = models.ForeignKey('Month', verbose_name='Miesiąc rozliczeniowy', on_delete=models.PROTECT)
+    biling_year = models.ForeignKey('Year', verbose_name='Rok rozliczeniowy', on_delete=models.PROTECT)
+    usage = models.FloatField(verbose_name='Zużycie licznika')
+
+    def __str__(self):
+        return f'''Zużycie licznika z {self.biling_year} {self.biling_month} w wysokości {self.usage}'''
+
+
+
--- administrative_costs/electricity_cost/urls.py
+++ administrative_costs/electricity_cost/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.http import HttpResponse
+from .models import Invoices
+from django.views.generic import ListView
 
 app_name = 'electricity_cost'
 
 urlpatterns = [
-    path("", HttpResponse('helo')),
+    path("faktury/", ListView.as_view(model=Invoices, template_name='electricity_cost/invoices_view.html'),
+         name='lista_faktur'),
 ]
