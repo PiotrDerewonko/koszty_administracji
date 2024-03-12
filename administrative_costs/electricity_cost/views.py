bb6f6513159b9e04de0efd44eeb601275fc7079b
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-03-12 22:23:31 +0100

    dodanie lekkiej stylizacji tabeli oraz sortowania po wszytskich kolumnach

--- administrative_costs/electricity_cost/views.py
+++ administrative_costs/electricity_cost/views.py
@@ -6,8 +6,8 @@ from django.core.paginator import Paginator
 class InvoicesListView(ListView):
     model = Invoices
     template_name = 'electricity_cost/invoices_view.html'
-    paginate_by = 2
-    ordering = ['id']  # Ustawienie domyślnego sortowania
+    paginate_by = 20
+    ordering = ['-id']  # Ustawienie domyślnego sortowania
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -20,4 +20,6 @@ class InvoicesListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        sort_param = self.request.GET.get('sort', 'cost')  # Domyślna wartość sortowania
+        context['sort'] = sort_param
         return context
