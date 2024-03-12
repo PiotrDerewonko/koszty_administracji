6045d0d54190bc1d68bb4b394a354a759ebac019
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-03-12 16:57:08 +0100

    poczatek prac nac ladniejszym html oraz sortujaca sie lista widokow

--- administrative_costs/electricity_cost/views.py
+++ administrative_costs/electricity_cost/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Invoices
 
-# Create your views here.
+class InvoicesListView(ListView):
+    model = Invoices
+    template_name = 'electricity_cost/invoices_view.html'
+    context_object_name = 'object_list'
+    ordering = ['id']  # Ustawienie domyślnego sortowania
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Pobranie wartości parametru sort z adresu URL
+        sort_param = self.request.GET.get('sort')
+        if sort_param:
+            # Wykonanie sortowania na podstawie parametru sort
+            queryset = queryset.order_by(sort_param)
+        return queryset
