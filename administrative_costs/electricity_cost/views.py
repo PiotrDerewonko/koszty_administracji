21ef0b9042af69e36de493491cd90856122d24b5
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-03-13 16:28:42 +0100

    dodanie mozliwosci logowania i wymagania logowania na widoku z fakturami

--- administrative_costs/electricity_cost/views.py
+++ administrative_costs/electricity_cost/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
 from .models import Invoices
 from django.core.paginator import Paginator
-
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+@method_decorator(login_required, name='dispatch')
 class InvoicesListView(ListView):
     model = Invoices
     template_name = 'electricity_cost/invoices_view.html'
