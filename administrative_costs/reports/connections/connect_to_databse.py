cd9aa7d4b3966f2375e1f692396c7930663f4e74
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-07-31 22:36:05 +0200

    pierwsze podejscei do raprotow

---
+++ administrative_costs/reports/connections/connect_to_databse.py
@@ -0,0 +1,13 @@
+import sqlite3
+import os
+
+
+def connect_to_databse(db_file):
+    full_path = os.path.abspath(db_file)
+    conn = sqlite3.connect(full_path)
+    if not os.path.exists(full_path):
+        print(f"Baza danych db.sqlite3 nie istnieje.")
+    else:
+        print(f"Łączenie z bazą danych: db.sqlite3")
+    return conn
+
