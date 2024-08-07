fac2367af66b7d393e4e5d77a4d93ee65cc74458
Piotr Derewonko <p.derewonko@fsaps.pl>
2024-08-07 15:52:16 +0200

    pierwsdzy commit z raportami

--- administrative_costs/reports/sql_queries/energy_meters_tree.sql
+++
@@ -1,5 +0,0 @@
-select main.name as licznik_main, submain.name as licznik_submain from electricity_cost_energymetertree tree
-left outer join electricity_cost_energymeters main
-on tree.energy_meter_main_id = main.id
-left outer join electricity_cost_energymeters submain
-on tree.energy_meter_submain_id = submain.id
